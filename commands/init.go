5a29bef325e468ad7f205b545e433867f3f7e581
chroju <chor.chroju@gmail.com>
2018-10-31 15:09:19 +0900

    Temporary: add syncing feature to init command

--- commands/init.go
+++ commands/init.go
@@ -6,11 +6,26 @@ import (
 	"os"
 	"os/user"
 
+	cloud "github.com/chroju/go-nature-remo/cloud"
+	"github.com/go-yaml/yaml"
 	"github.com/mitchellh/cli"
 )
 
 type InitCommand struct {
 	UI cli.Ui
+}
+
+type Appliance struct {
+	Name    string
+	ID      string
+	Signals []cloud.Signal
+}
+
+type Setting struct {
+	Credential struct {
+		Token string
+	}
+	Appliances []Appliance
 }
 
 func (c *InitCommand) Run(args []string) int {
@@ -29,6 +44,8 @@ func (c *InitCommand) Run(args []string) int {
 	c.UI.Output("Nature Remo OAuth Token:")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
+		token := scanner.Text()
+
 		if _, err := os.Stat(configDir); err == os.ErrNotExist {
 			if err := os.Mkdir(configDir, 0755); err != nil {
 				panic(err)
@@ -40,7 +57,24 @@ func (c *InitCommand) Run(args []string) int {
 		}
 		defer file.Close()
 
-		io.WriteString(file, scanner.Text())
+		c.UI.Output("Initializing ...")
+
+		client := cloud.NewClient(token)
+		appliances, err := client.GetAppliances()
+		if err != nil {
+			c.UI.Error(err.Error())
+			return 1
+		}
+		s := Setting{}
+		s.Credential.Token = token
+		for _, a := range appliances {
+			s.Appliances = append(s.Appliances, Appliance{Name: a.Nickname, ID: a.ID, Signals: a.Signals})
+		}
+
+		y, err := yaml.Marshal(&s)
+		io.WriteString(file, string(y))
+
+		c.UI.Output("Successfully initialized.")
 		break
 	}
 
